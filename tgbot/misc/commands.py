1c6c73a86a5abc5b1d902dc3644bcb5a9feb703f
EnverMT <anvarmtg@gmail.com>
2023-02-28 17:03:53 +0500

    refactored

--- tgbot/misc/commands.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import BotCommand, BotCommandScopeChat
-from aiogram import Bot
-
-async def set_menu_for_unregistered_users(bot : Bot):
-    return await bot.set_my_commands(commands=[BotCommand('start', 'Старт бота'),
-                                               BotCommand('register', 'Пройти регистрацию'),
-                                               BotCommand('cancel', 'Отмена')])
