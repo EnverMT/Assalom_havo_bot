6b0a2c41f980829e67f5b87e313a9c89dc038e56
EnverMT <anvarmtg@gmail.com>
2023-02-28 12:04:22 +0500

    admin approve info expanded

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -8,7 +8,7 @@ from sqlalchemy import select, update, delete
 
 from tgbot.keyboards.inline import AdminMenu
 from tgbot.misc.states import AdminState
-from tgbot.models.models import User, Phone
+from tgbot.models.models import User, Phone, Address
 from tgbot.services.DbCommands import DbCommands
 
 db = DbCommands()
@@ -17,8 +17,6 @@ db = DbCommands()
 async def admin_start(message: Message):
     await AdminState.Menu.set()
     return await message.reply("Hello, admin!", reply_markup=AdminMenu)
-
-
 async def list_of_waiting_approval_users(call: types.CallbackQuery, state: FSMContext):
     await call.message.edit_reply_markup()
     users = await db.get_list_of_waiting_approval_users(call=call)
@@ -34,31 +32,34 @@ async def list_of_waiting_approval_users(call: types.CallbackQuery, state: FSMCo
     await call.bot.send_message(chat_id=call.from_user.id, text="List of waiting approval users",
                                 reply_markup=inline_user_keyboard)
     await AdminState.ListOfWaitingApprovalUsers.set()
-
-
 async def waiting_approval_user(call: types.CallbackQuery, state: FSMContext):
     await call.message.edit_reply_markup()
     user: User = await db.select_user(call=call, user_id=int(call.data))
     text = f"Name: {user.full_name}\n"
 
     db_session = call.bot.get("db")
-    sql = select(Phone).where(Phone.user_id == int(user.id))
-    async with db_session() as session:
-        result = await session.execute(sql)
-        rows: List[Tuple[Phone]] = result.all()
-        if rows:
-            for r in rows:
-                text += f"Tel: {r[0].numbers}"
+    sql_phone = select(Phone).where(Phone.user_id == int(user.id))
+    sql_addresses = select(Address).where(Address.user_id == int(user.id))
 
-    keyb = InlineKeyboardMarkup(row_width=1)
-    keyb.add(InlineKeyboardButton(text="Принять", callback_data="Approve"))
-    keyb.add(InlineKeyboardButton(text="Отказать", callback_data="Deny"))
+    async with db_session() as session:
+        phones = await session.execute(sql_phone)
+        addresses = await session.execute(sql_addresses)
+        phone_list: List[Tuple[Phone]] = phones.all()
+        addresses_list: List[Tuple[Address]] = addresses.all()
+        if phone_list:
+            for phone_tuple in phone_list:
+                text += f"Tel: {phone_tuple[0].numbers}\n"
+        if addresses_list:
+            for addr_tuple in addresses_list:
+                text += f"Address: {addr_tuple[0].house}/{addr_tuple[0].apartment}\n"
+
+    keyboard = InlineKeyboardMarkup(row_width=1)
+    keyboard.add(InlineKeyboardButton(text="Принять", callback_data="Approve"))
+    keyboard.add(InlineKeyboardButton(text="Отказать", callback_data="Deny"))
 
     await AdminState.WaitingApproval.set()
     await state.update_data(user_id=int(user.id))
-    await call.bot.send_message(chat_id=call.from_user.id, text=text, reply_markup=keyb)
-
-
+    await call.bot.send_message(chat_id=call.from_user.id, text=text, reply_markup=keyboard)
 async def approve_user(call: types.CallbackQuery, state: FSMContext):
     db_session = call.bot.get("db")
     user_data = await state.get_data()
@@ -81,10 +82,6 @@ async def approve_user(call: types.CallbackQuery, state: FSMContext):
             await session.execute(sql)
             await session.commit()
         await call.bot.send_message(chat_id=call.from_user.id, text="Denied")
-
-
-
-
 def register_admin(dp: Dispatcher):
     dp.register_message_handler(admin_start, commands=["start"], state="*", is_admin=True)
     dp.register_callback_query_handler(list_of_waiting_approval_users,
