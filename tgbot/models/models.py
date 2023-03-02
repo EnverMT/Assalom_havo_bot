2989864e4f6b88849db2f4d80680256821a3b029
EnverMT <anvarmtg@gmail.com>
2023-03-02 11:24:31 +0500

    propiska foreign key cascade delete removed

--- tgbot/models/models.py
+++ tgbot/models/models.py
@@ -109,7 +109,7 @@ class Propiska(Base):
 
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id", onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
-    address_id = Column(Integer, ForeignKey("address.id", onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
+    address_id = Column(Integer, ForeignKey("address.id", onupdate="CASCADE"), nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
