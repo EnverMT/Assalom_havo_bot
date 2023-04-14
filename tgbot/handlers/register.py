8bce3c21ea966bacf41fd8351f190739186de275
Enver <enver@git.com>
2023-04-15 00:15:28 +0500

    register user

--- tgbot/handlers/register.py
+++ tgbot/handlers/register.py
@@ -8,7 +8,6 @@ from sqlalchemy import insert, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from bot import bot
-from tgbot.keyboards.reply import contact_request
 from tgbot.misc.states import RegisterState
 from tgbot.models import *
 from tgbot.services.DbCommands import DbCommands
@@ -40,49 +39,53 @@ async def check_register_status(message: types.Message, state: FSMContext, sessi
     await state.set_state(RegisterState.ReadyToRegister)
 
 
+@router.message(RegisterState.ReadyToRegister)
 async def register_get_fio(message: types.Message, state: FSMContext):
-    await RegisterState.fio.set()
-    await state.update_data(fio=html(message.text))
+    await state.set_state(RegisterState.fio)
+    await state.update_data(fio=html.quote(message.text))
 
     builder = ReplyKeyboardBuilder()
     builder.row(
-        types.KeyboardButton(text="Запросить геолокацию", request_location=True),
-        types.KeyboardButton(text="Запросить контакт", request_contact=True)
+        types.KeyboardButton(text="Ваш Контакт.", request_contact=True)
     )
 
     await message.answer(text=f"Прошу предоставит ваш телефон Контакт.",
-                         reply_markup=contact_request)
+                         reply_markup=builder.as_markup(resize_keyboard=True))
 
 
+@router.message(RegisterState.fio, F.contact)
 async def register_get_contact(message: types.Message, state: FSMContext):
-    await RegisterState.phone_number.set()
+    if (message.from_user.id != message.contact.user_id):
+        await state.clear()  # TO DO: Ban such cheating users
+        return
+    await state.set_state(RegisterState.phone_number)
     await state.update_data(user_phone=message.contact.phone_number)
-
     await message.answer(f"Ввведите номер вашего дома: (Только цифрами)", reply_markup=types.ReplyKeyboardRemove())
-    await RegisterState.address_house.set()
 
 
+@router.message(RegisterState.phone_number)
 async def register_get_address_house(message: types.Message, state: FSMContext):
+    await state.set_state(RegisterState.address_house)
     if not message.text.isnumeric():
         await message.reply(text="Введите корректный номер дома")
         return
 
-    house_num = int(html(message.text))
+    house_num = int(html.quote(message.text))
     if house_num > 49 or house_num < 42:
         await message.reply(text="Введите корректный номер дома")
         return
     await state.update_data(user_address_house=house_num)
-
-    await RegisterState.address_apartment.set()
     await message.answer(f"Ввведите номер вашей квартиры: (Только цифрами)", reply_markup=types.ReplyKeyboardRemove())
 
 
-async def register_get_address_apartment(message: types.Message, state: FSMContext):
+@router.message(RegisterState.address_house)
+async def register_get_address_apartment(message: types.Message, state: FSMContext, session: AsyncSession):
+    await state.set_state(RegisterState.address_apartment)
     if not message.text.isnumeric():
         await message.reply(text="Введите корректный номер квартиры")
         return
 
-    apartment_num = int(html(message.text))
+    apartment_num = int(html.quote(message.text))
     if apartment_num > 90 or apartment_num < 0:
         await message.reply(text="Введите корректный номер квартиры")
         return
@@ -91,10 +94,9 @@ async def register_get_address_apartment(message: types.Message, state: FSMConte
 
     sql_get_address_id = select(Address).where(Address.house == int(user_data['user_address_house']),
                                                Address.apartment == int(user_data['user_address_apartment']))
-    db_session = message.bot.get("db")
-    async with db_session() as session:
-        address_res = await session.execute(sql_get_address_id)
-        address: List[Address] = address_res.first()
+
+    address_res = await session.execute(sql_get_address_id)
+    address: List[Address] = address_res.first()
     if not address:
         await message.reply(text="Адрес не существует в базе")
         return
@@ -109,31 +111,16 @@ async def register_get_address_apartment(message: types.Message, state: FSMConte
     await message.answer(f"Домком может Вам позвонить по номеру {user_data['user_phone']} для уточнения деталей")
     await message.answer(f"Если Ваш телеграм номер недоступна для входящих звонков, могут быть задержки одобрения")
 
-    current_user = await db.select_current_user(message=message)
+    current_user = await db.select_current_user(message=message, session=session)
     sql_phone = insert(Phone).values(numbers=user_data['user_phone'],
                                      user_id=current_user.id)
     sql_propiska = insert(Propiska).values(user_id=current_user.id,
                                            address_id=address[0].id)
     sql_user = update(User).values(fio=user_data['fio']).where(User.telegram_id == message.from_user.id)
 
-    async with db_session() as session:
-        await session.execute(sql_phone)
-        await session.execute(sql_propiska)
-        await session.execute(sql_user)
-        await session.commit()
+    await session.execute(sql_phone)
+    await session.execute(sql_propiska)
+    await session.execute(sql_user)
+    await session.commit()
 
-    await state.finish()
-    await state.reset_state()
-
-
-def register_register_menu(dp: Dispatcher):
-    dp.register_message_handler(check_register_status, commands=["register"], state='*',
-                                chat_type=types.ChatType.PRIVATE)
-    dp.register_message_handler(register_get_fio, state=RegisterState.ReadyToRegister)
-    dp.register_message_handler(register_get_contact,
-                                state=RegisterState.fio,
-                                content_types=types.ContentType.CONTACT)
-    dp.register_message_handler(register_get_address_house,
-                                state=RegisterState.address_house)
-    dp.register_message_handler(register_get_address_apartment,
-                                state=RegisterState.address_apartment)
+    await state.clear()
